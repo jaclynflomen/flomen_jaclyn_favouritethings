672caeceda78e7d60c5a748251024deafbb52e88
jaclynflomen <j_floman@fanshaweonline.ca>
2019-03-28 16:40:22 -0400

    update

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+var connect = require('../utils/sqlConnect');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //do a database query and get some of the tbl_favs data
+  //do a database query and get some of the hero data
   connect.query(`SELECT name, image FROM tbl_favs`, (err, result) => {
     if (err) {
       throw err;
       console.log(err);
     }else{
       console.log(result);
-      res.render('index', { image: result });
+      res.render('index', { images: result });
     }
   });
 });
 
 //get individual data / bio info
-router.get('/:thing', function(req, res, next) {
-  //do a database query and get some of the hero data
-  connect.query(`SELECT * FROM tbl_favs WHERE name="${req.params.thing}"`, (err, result) => {
+router.get('/:fav', function(req, res, next) {
+  //do a database query and get some of the tbl_favs data
+  connect.query(`SELECT * FROM tbl_favs WHERE name="${req.params.fav}"`, (err, result) => {
     if (err) {
       throw err;
       console.log(err);
